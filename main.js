2229d235494607e363e6b17016f09ff3c0a9b3ae
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2022-02-17 17:51:22 -0800

    down
    up

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function setup()
     video.size(300, 300);
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on("pose", gotPoses);
+    poseNet.go('pose', gotPoses);
 }
 function draw()
 {
@@ -22,14 +22,14 @@ function takeSnapshot()
 }
 function modelLoaded()
 {
-    console.log("PostNet is initialized.");
+    console.log("PoseNet is Initialized.");
 }
 function gotPoses(results)
 {
     if(results.length > 0)
     {
         console.log(results);
-        console.log("pose x = " + results[0].pose.nose.x);
-        console.log("pose y = " + results[0].pose.nose.y);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
     }
 }
